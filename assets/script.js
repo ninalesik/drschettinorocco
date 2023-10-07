23a1fb32b3a403aee936e194a8f479889840dca3
Nina Kisel <ninakisel@gmail.com>
2023-10-07 22:44:18 +0200

    Media query, cookie notification added, burger menu anchors fixed

--- assets/script.js
+++ assets/script.js
@@ -3,21 +3,32 @@ document.querySelector(".burger").addEventListener("click", function () {
   document.querySelector(".nav").classList.toggle("open");
 });
 
-// Swiper slider
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+      });
+
+      document.querySelector(".burger").classList.remove("active");
+      document.querySelector(".nav").classList.remove("open");
+    });
+  });
+});
+
 const swiper = new Swiper(".swiper", {
-  // Optional parameters
   direction: "horizontal",
   loop: true,
   speed: 1000,
   effect: "fade",
 
-  // If we need pagination
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
 
-  // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -27,3 +38,14 @@ const swiper = new Swiper(".swiper", {
     delay: 2000,
   },
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  if (!localStorage.getItem("cookieAccepted")) {
+    document.getElementById("cookiePopup").style.display = "block";
+  }
+});
+
+function closeCookiePopup() {
+  document.getElementById("cookiePopup").style.display = "none";
+  localStorage.setItem("cookieAccepted", true);
+}
